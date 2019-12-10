0c3c5d7dfd6ae617320b265adf511fa24b59fe18
Lutetium-Vandadium <luv.s7000@gmail.com>
2019-12-10 11:41:20 +0530

    ocr

--- text_from_im.py
+++ text_from_im.py
@@ -46,7 +46,7 @@ def text_from_image_path(image_path):
             cv.imshow('TB', text_box)
             cv.waitKey(0)
 
-        config = ("-l eng +equ --oem 1 --psm 4")
+        config = ("-l equ --oem 1 --psm 4")
         text = pytesseract.image_to_string(text_box, config=config)
         log(text)
         return text
@@ -65,18 +65,14 @@ def text_from_image(image):
     else:
         text_box = cv.cvtColor(text_box, cv.COLOR_BGR2GRAY)
 
-        _, text_box = cv.threshold(
-            text_box, 0, 255, cv.THRESH_OTSU+cv.THRESH_BINARY)
+        _, text_box = cv.threshold(text_box, 0, 255, cv.THRESH_OTSU + cv.THRESH_BINARY)
+        # text_box = cv.adaptiveThreshold(text_box, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, min(text_box.shape[:2]), 0)
 
         if TESTING:
             cv.imshow('TB', text_box)
             cv.waitKey(0)
-        # in order to apply Tesseract v4 to OCR text we must supply
-        # (1) a language, (2) an OEM flag of 4, indicating that the we
-        # wish to use the LSTM neural net model for OCR, and finally
-        # (3) an OEM value, in this case, 7 which implies that we are
-        # treating the ROI as a single line of text
-        config = ("-l eng --oem 1 --psm 4")
+
+        config = ("-l equ --oem 1 --psm 4")
         text = pytesseract.image_to_string(text_box, config=config)
         log(text)
         return text
